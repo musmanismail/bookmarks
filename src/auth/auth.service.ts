79e8c8a8d9245bdb9317d618cdb95a2b4a9ce0fc
Muhammad Usman <muhammadusmanismail99@gmail.com>
2024-11-14 23:32:19 +0500

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+@Injectable()
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  signin() {
+    return {
+      message: 'Signin',
+      status: 'success',
+    };
+  }
+
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+
+    return user;
+  }
+}
